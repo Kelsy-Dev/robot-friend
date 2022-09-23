2ab4b45b2f0248abf9904469301a485ab1053666
KLC <mkelsy@icloud.com>
2022-09-23 02:05:13 -0400

    Adding tachyons style to the cards

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Card = () => {
     return (
-        <div> 
-            <img alt="Robot" src='https://robohash.org/test' />
+        <div className='bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'> 
+            <img alt="Robot" src='https://robohash.org/test?200x200' />
             <div>
             <h1>Just a test</h1>
             <p>This is a robot</p>
--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ import 'tachyons'
 import reportWebVitals from './reportWebVitals';
 
 
-ReactDOM.render(<Card />, document.getElementById('root'));
+
+
+ReactDOM.render(
+    <div>
+    <Card />
+    <Card />
+    <Card />
+    </div>,
+    document.getElementById('root'));
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
 //   <React.StrictMode>
