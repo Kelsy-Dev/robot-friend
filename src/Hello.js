3fd961d0f44ee8012fd595f56a6d8adb3f7dafd4
KLC <mkelsy@icloud.com>
2022-09-22 23:37:16 -0400

    Adding a repo

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+// import React, { component } from "react";
+
+// class Hello extends React.component {
+//     render() {
+//         return 
+//         <div>
+//             <h1> Hello world</h1>
+//             <p>Welcome to react</p>
+//         </div>
+//     }
+// }
+
+// export default Hello;
